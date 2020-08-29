92263feb1d6a21c71d9fb474a2f673902002a329
unknown <mail2ishanshah@gmail.com>
2020-08-29 11:01:42 +0530

    WIP in memory data service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http'
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentsComponent } from './students/students.component';
 import { StudentDetailComponent } from './student-detail/student-detail.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { InMemoryDataService } from './in-memory-data.service';
 
 @NgModule({
   declarations: [
@@ -18,7 +22,9 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/student-data.service.ts
+++ src/app/student-data.service.ts
@@ -2,17 +2,26 @@ import { Injectable } from '@angular/core';
 import { iStudent } from './iStudent';
 import { STUDENTS } from './mock-students';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentDataService {
 
-  constructor() { }
+  private apiUrl = 'api/students'; // URL for web API
+  constructor(
+    private http: HttpClient
+  ) { }
 
   getStudents() : Observable<iStudent[]> {
+    // return student without calling API via HTTP call
+    // return of(STUDENTS);
     // call your API service via http call
-    return of(STUDENTS);
+// In web API
+// When you want to read data use get operation
+   
+    return this.http.get<iStudent[]>(this.apiUrl);
   }
 
   // returns student data as per the input  -> return perticular data
